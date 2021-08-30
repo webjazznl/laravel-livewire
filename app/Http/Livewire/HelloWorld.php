28b8868d35c9c3bf8e1ba3b0ab53ddef5fb77675
webjazznl <hsiebelink73@gmail.com>
2021-08-30 11:57:27 +0200

    Mount and hydrate method

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -9,9 +9,18 @@ class HelloWorld extends Component
 {
     public $name = 'Jelly';
 
+    public $flower = '.';
+
+    //Runs on the first request
     public function mount(Request $request, $name = 'Bar')
     {
         $this->name = $request->input('name', $name);
+    }
+
+    //runs every subsequent ajax request
+    public function hydrate()
+    {
+        $this->flower .= '.';
     }
 
     public function render()
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,3 +1,5 @@
 <div>
-  Hello {{ $name }}
+    <input type="text" name="name" id="name" wire:model="name">
+  Hello {{ $name }} <br>
+  {{ $flower }}
 </div>
