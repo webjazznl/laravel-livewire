b20c6220a8d330fd8a612d2ccf2e1fb59aeb7a33
webjazznl <hsiebelink73@gmail.com>
2021-08-30 14:24:40 +0200

    Simple delete example

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Contact;
 use Livewire\Component;
 use Illuminate\Http\Request;
 
 class HelloWorld extends Component
 {
-    public $names = ['Foo', 'Bar', 'Batz'];
+    public $contacts;
 
+    public function mount()
+    {
+        $this->contacts=Contact::all();
+    }
+
+    public function removeContact($id)
+    {
+        Contact::destroy($id);
+        $this->contacts = Contact::all();
+    }
     public function render()
     {
         return view('livewire.hello-world');
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,5 +1,12 @@
 <div>
-    @foreach ($names as $name )
-        @livewire('say-hi', ['name' => $name])
+    @foreach ($contacts as $contact )
+    <div>
+        @livewire('say-hi', ['name' => $contact->name], key($contact->id))
+        <button wire:click="removeContact({{$contact->id}})">Remove</button> {{now()}}
+    </div>
     @endforeach
+
+    <button wire:click="$refresh">Refresh</button> {{now()}}
+
+
 </div>
