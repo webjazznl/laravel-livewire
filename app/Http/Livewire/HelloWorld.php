69df4f86a53db2949beaa890ff778210e826a8a8
webjazznl <hsiebelink73@gmail.com>
2021-08-30 11:35:21 +0200

    Clean Up

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -8,14 +8,6 @@ class HelloWorld extends Component
 {
     public $name = 'Jelly';
 
-    public $loud = false;
-
-    public $greeting = ['Hello'];
-
-    public function resetName($name ='Chico')
-    {
-        $this->name = $name;
-    }
 
     public function render()
     {
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,18 +1,3 @@
 <div>
-    <input type="text" wire:model.lazy="name">
-    <input type="checkbox" wire:model="loud">
-    <select multiple wire:model="greeting">
-        <option value="Hi">Hi</option>
-        <option value="Hello">Hello</option>
-        <option value="Good bye">Good bye</option>
-    </select>
-
-    {{ implode(', ', $greeting) }} {{ $name }}
-
-    @if ($loud)
-        !!!
-    @endif
-    <form action='#' wire:submit.preventDefault="$set('name', 'Bingo')">
-    <button >Reset me</button>
-    </form>
+  Hello {{ $name }}
 </div>
